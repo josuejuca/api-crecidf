88e64b82cfbe9af3838c18302dd71ddd9be4a1a0
josuejuca <josuejuca19@gmail.com>
2024-09-17 01:59:25 -0300

    [17/09] Atualização: Buscar Corretor

--- app/api.py
+++ app/api.py
@@ -1,5 +1,4 @@
-# api.py 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 import requests
 
 app = FastAPI()
@@ -10,24 +9,37 @@ CRECI_API_HOST = "https://crecidf-api-002-2d82d2b4dcc1.herokuapp.com"
 def home():
     return {"message": "API CRECI DF | JUCA"}
 
-# Endpoint para receber o número CRECI e retornar as informações
 @app.post("/creci_info/", tags=["CRECI – DF"])
 def get_creci_info(creci_number: str):
-    # 1. Transformar o número CRECI em userId
     user_id = get_user_id_by_creci_number(creci_number)
     
     if not user_id:
         raise HTTPException(status_code=404, detail="CRECI não encontrado.")
     
-    # 2. Validar o userId
     creci_data = verify_certificate(user_id)
     
     if not creci_data:
         raise HTTPException(status_code=404, detail="Erro ao validar o CRECI.")
     
-    # 3. Incluir o userId nos dados de resposta e retornar os dados coletados
     creci_data["userId"] = user_id
     return creci_data
+
+@app.post("/buscar_corretor/", tags=["CRECI – DF"])
+def buscar_corretor(consulta: str = Query(..., description="Número de CRECI, nome, CPF ou CNPJ do corretor")):
+    try:
+        url = f"{CRECI_API_HOST}/findBroker"
+        headers = {"Content-Type": "application/json"}
+        data = {"query": consulta}  # Enviando o valor no formato correto com 'query' como chave
+
+        response = requests.post(url, headers=headers, json=data)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise HTTPException(status_code=response.status_code, detail="Erro ao buscar informações do corretor.")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro interno: {str(e)}")
 
 def get_user_id_by_creci_number(creci_number: str) -> str:
     url = f"{CRECI_API_HOST}/getUserIdByCreciNumber"
